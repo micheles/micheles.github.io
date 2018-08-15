6b0f916258b8a2707aef2c49f3d51c7ba9f02ca8
Michele Simionato <michele.simionato@gmail.com>
2018-08-15 06:24:29 +0200

    Removed italian stuff

--- public/cose/mazinger/x.py
+++
@@ -1,9 +0,0 @@
-import re
-
-begin = []
-end = []
-
-for line in open('riassunti.txt'):
-    begin.append(line[:40].strip())
-    end.append(line[41:].strip())
-print re.sub('(\d+\.)', r'\n\n\1', ' '.join(begin + end))
